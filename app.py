f8e6cda31992ff3544b1d7d3c6c2081592093601
= <abdoahmedsayed704@gmail.com>
2024-08-20 07:11:16 +0300

    Done..

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ app.register_blueprint(app_blueprint)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo " ==== You need us :) ==== "
+sudo apt update
+sudo apt install sqlite3
 pip install flask_login
 pip install flask_sqlalchemy
 pip install flask_wtf
 pip install flask_bcrypt
 pip install wtforms
+pip install sqlite3
