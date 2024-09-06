ac35f9b62ae2b8942f754dfedf7519beee28d51f
= <abdoahmedsayed704@gmail.com>
2024-09-06 17:59:30 +0300

    Testinf

--- frontend/src/computer/hard_comp.js
+++ frontend/src/computer/hard_comp.js
@@ -1,7 +1,7 @@
 // const [lichessToken] = useState('lip_ECM6encOKT0kEivL4A17');  // Replace with your Lichess API token
 
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
 import '../css-files/offline.css';
